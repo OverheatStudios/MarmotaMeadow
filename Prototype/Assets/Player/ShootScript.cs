c617d9f1189973598beda8ba718e084db9794558
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-02-05 11:12:53 +0000

    hitscan

--- Prototype/Assets/Player/ShootScript.cs
+++ Prototype/Assets/Player/ShootScript.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class ShootScript : MonoBehaviour
 {
     [SerializeField] private float m_cooldown = 0.4f;
     private float m_currentCooldown = 0;
-    [SerializeField] private GameObject m_bulletPrefab;
+    [SerializeField] private LayerMask m_enemyLayer;
     [SerializeField] private GameObject m_camera;
-    [SerializeField] private float m_bulletSpeed = 15;
+    [SerializeField] private float m_maxDistance = 100.0f;
+    [SerializeField] private DataScriptableObject m_data;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        Assert.IsNotNull(m_camera);
+        Assert.IsNotNull(m_data);
     }
 
     void Update()
@@ -24,13 +28,27 @@ public class ShootScript : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject bullet = Instantiate(m_bulletPrefab);
-            bullet.transform.position = transform.position;
-            bullet.transform.rotation = m_camera.transform.rotation;
-            bullet.transform.rotation *= Quaternion.Euler(0, 90, 0);
-            BulletScript bulletScript = bullet.GetComponent<BulletScript>();
-            bulletScript.m_velocity = m_camera.transform.forward * m_bulletSpeed;
+            ShootBullet();
             m_currentCooldown = m_cooldown;
         }
+    }
+
+    void ShootBullet()
+    {
+        Ray ray = new Ray(m_camera.transform.position, m_camera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, m_maxDistance, m_enemyLayer))
+        {
+            GroundhogScript groundhogScript = hit.collider.gameObject.GetComponent<GroundhogScript>();
+            Assert.IsNotNull(groundhogScript);
+            KillGroundhog(groundhogScript);
+        }
+    }
+
+    void KillGroundhog(GroundhogScript groundhog)
+    {
+        Destroy(groundhog.gameObject);
+        m_data.groundhogsKilled++;
     }
 }
