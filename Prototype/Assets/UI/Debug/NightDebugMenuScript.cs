a335b75dfe631645127e84b8443bf262eee644eb
jack183190 <119117553+jack183190@users.noreply.github.com>
2025-02-25 12:49:30 +0000

    multiple guns

--- Prototype/Assets/UI/Debug/NightDebugMenuScript.cs
+++ Prototype/Assets/UI/Debug/NightDebugMenuScript.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class NightDebugMenu : MonoBehaviour
 {
-   
+    [SerializeField] private InventoryMager m_inventoryManager;
+    [SerializeField] private BaseItem m_shotgunPrefab;
+    [SerializeField] private BaseItem m_pistolPrefab;
+    [SerializeField] private BaseItem m_riflePrefab;
+
+    public void GiveShotgun()
+    {
+        m_inventoryManager.AddItem(m_shotgunPrefab);
+    }
+
+    public void GivePistol()
+    {
+        m_inventoryManager.AddItem(m_pistolPrefab);
+    }
+
+    public void GiveRifle()
+    {
+        m_inventoryManager.AddItem(m_riflePrefab);
+    }
 }
