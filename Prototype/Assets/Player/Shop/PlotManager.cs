a1342c90061bd8d79bd88d6928fb1afd04468996
FLYHAPY <eduardospinola04@gmail.com>
2025-02-25 12:10:26 +0000

    added option on shop to buy more plots

---
+++ Prototype/Assets/Player/Shop/PlotManager.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[System.Serializable]
+public class PlotData
+{
+    public int numberOfPlots = 1;
+}
+
+public class PlotManager : MonoBehaviour
+{
+    
+    [SerializeField] private string m_saveLocation;
+    private string filePath;
+    [SerializeField] private List<GameObject> m_plots;
+    [SerializeField] private PlotData plots;
+    [SerializeField] private int numberOfPlots;
+    [SerializeField] private GameObject m_plotPrefab;
+
+    void Start()
+    {
+        filePath = Application.dataPath + "/" + m_saveLocation;
+        Load();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Save();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            IncreaseNumberOfPlots();
+        }
+    }
+    
+    private void Save()
+    {
+        plots.numberOfPlots = numberOfPlots;
+        string json = JsonUtility.ToJson(plots, true);
+        File.WriteAllText(filePath, json);
+    }
+
+    private void Load()
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            PlotData data = JsonUtility.FromJson<PlotData>(json);
+            
+            if (data != null) 
+            {
+                numberOfPlots = data.numberOfPlots;
+            }
+
+            if (SceneManager.GetActiveScene().name == "Day Scene")
+            {
+                for (int i = 0; i < numberOfPlots; i++)
+                {
+                    Instantiate(m_plotPrefab, m_plots[i].transform);
+                }
+            }
+        }
+    }
+
+    public void IncreaseNumberOfPlots()
+    {
+        numberOfPlots++;
+    }
+}
---
+++ Prototype/Assets/Player/Shop/Shop.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] InventoryMager inventoryMager;
+    [SerializeField] private TextMeshProUGUI CoinsText;
+    [SerializeField] private PlotManager plotManager;
+
+    public void BuyItem(Seeds item)
+    {
+        inventoryMager.AddItem(item);
+        inventoryMager.DecreaseCoins(item.ReturnBuyCoinsAmount());
+        CoinsText.text = "Coins: " + inventoryMager.GetCoins();
+    }
+
+    public void SellItem(InventorySlot item)
+    {
+        if (item.GetComponentInChildren<InventoryItem>().item is Crops crop)
+        {
+            inventoryMager.IncreaseCoins(crop.ReturnSellCoinsAmount());
+            item.GetComponentInChildren<InventoryItem>().DecreaseAmount();
+            CoinsText.text = "Coins: " + inventoryMager.GetCoins();
+        }
+    }
+
+    public void UpgradeTool(InventorySlot item)
+    {
+        if (item.GetComponentInChildren<InventoryItem>().item is Tool tool)
+        {
+            inventoryMager.DecreaseCoins(tool.ReturnToolLevelCost());
+            item.GetComponentInChildren<InventoryItem>().IncreaseMultiplier();
+            CoinsText.text = "Coins: " + inventoryMager.GetCoins();
+        }
+    }
+
+    public void BuyPlot(int money)
+    {
+        plotManager.IncreaseNumberOfPlots();
+        inventoryMager.DecreaseCoins(money);
+        CoinsText.text = "Coins: " + inventoryMager.GetCoins();
+    }
+}
