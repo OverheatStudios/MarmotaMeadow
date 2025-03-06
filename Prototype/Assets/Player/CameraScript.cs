0126e74d19e7ff6bd620425049c30e51d24e724b
jack183190 <119117553+jack183190@users.noreply.github.com>
2025-03-06 15:07:01 +0000

    Fixed build errors

--- Prototype/Assets/Player/CameraScript.cs
+++ Prototype/Assets/Player/CameraScript.cs
@@ -1,5 +1,5 @@
-using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.InputSystem;
 
 public class CameraScript : MonoBehaviour
--- Prototype/Assets/UI/ReloadAnimation/ReloadAnimation.cs
+++ Prototype/Assets/UI/ReloadAnimation/ReloadAnimation.cs
@@ -1,7 +1,7 @@
-using NUnit.Framework;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.UI;
 
 public class ReloadAnimation : MonoBehaviour
