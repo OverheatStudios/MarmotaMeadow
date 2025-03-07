d646e8726df9bdd091bde90a55bcceaea1aa40e0
underscore95 <107675637+underscore95@users.noreply.github.com>
2025-03-07 09:26:17 +0000

    save cheat menu state in playerprefs

--- Prototype/Assets/UI/Debug/NightDebugMenuScript.cs
+++ Prototype/Assets/UI/Debug/NightDebugMenuScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NightDebugMenu : MonoBehaviour
 {
@@ -11,6 +13,18 @@ public class NightDebugMenu : MonoBehaviour
     [SerializeField] private BaseItem m_shotgunAmmo;
     [SerializeField] private BaseItem m_pistolAmmo;
     [SerializeField] private ShootScript m_shootScript;
+    [SerializeField] private Toggle m_infiniteAmmoToggle;
+    [SerializeField] private Toggle m_infiniteHealthToggle;
+    [SerializeField] private DataScriptableObject m_data;
+
+    private void Start()
+    {
+        m_infiniteAmmoToggle.isOn = PlayerPrefs.GetInt("infiniteAmmo", 0) != 0;
+        m_shootScript.SetInfiniteAmmoCheat(m_infiniteAmmoToggle);
+
+        m_infiniteHealthToggle.isOn = PlayerPrefs.GetInt("infiniteHealth", 0) != 0;
+        m_data.SetInfiniteHealthCheatStatus(m_infiniteHealthToggle);
+    }
 
     public void GiveShotgun()
     {
@@ -40,5 +54,15 @@ public class NightDebugMenu : MonoBehaviour
     public void SetAmmo(int ammo)
     {
         m_shootScript.SetAmmo(ammo);
+    }
+
+    public void UpdateAmmoPref(Toggle toggle)
+    {
+        PlayerPrefs.SetInt("infiniteAmmo", toggle.isOn ? 1 : 0);
+    }
+
+    public void UpdateHealthPref(Toggle toggle)
+    {
+        PlayerPrefs.SetInt("infiniteHealth", toggle.isOn ? 1 : 0);
     }
 }
