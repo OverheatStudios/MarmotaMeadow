a513a4ecd479c2e055bce514aa1950b09e632612
jack183190 <119117553+jack183190@users.noreply.github.com>
2025-03-13 14:20:01 +0000

    removed some random save words, added plants

--- Prototype/Assets/Environment/Billboard.cs
+++ Prototype/Assets/Environment/Billboard.cs
@@ -7,10 +7,20 @@ public class Billboard : MonoBehaviour
     [SerializeField] private SpriteRenderer m_image1;
     [SerializeField] private SpriteRenderer m_image2;
     [SerializeField] private Sprite m_sprite;
+    [SerializeField] private bool m_randomiseRotation = true;
 
     private void Start()
     {
         m_image1.sprite = m_sprite;
         m_image2.sprite = m_sprite;
+
+        if (m_randomiseRotation)
+        {
+            var rot = transform.rotation.eulerAngles;
+            rot.y = Random.Range(0f, 360f);
+            transform.rotation = Quaternion.Euler(rot);
+        }
+
+        transform.position += m_image1.size.y / 2 * Vector3.up;
     }
 }
